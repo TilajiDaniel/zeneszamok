df62d7de4acbc0f3bce0465d158446eaee0d4fe2
TilajiDG <tilajid@kkszki.hu>
2025-09-03 11:57:53 +0200

    minden jo

--- zeneszamok/Program.cs
+++ zeneszamok/Program.cs
@@ -9,28 +9,70 @@ namespace zeneszamok
         static MySqlConnection SQLConnection;
         static void Main(string[] args)
         {
-            BuildConnection();
-            SQLConnection.Open();
-            string sql = "SELECT * FROM eloado";
-            MySqlCommand cmd = new MySqlCommand();
-            cmd.CommandText = sql;
-            cmd.Connection = SQLConnection;
-            MySqlDataReader reader = cmd.ExecuteReader();
-            List<Eloado> eloadoLista = new List<Eloado>();
-            while (reader.Read())
-            {
-                Eloado eloado = new Eloado();
-                eloado.Id = reader.GetInt32("Id");
-                eloado.Nev = reader.GetString("Nev");
-                if (!reader.IsDBNull(reader.GetOrdinal("Nemzetiseg")))
-                {
-                    eloado.Nemzetiseg = reader.GetString("Nemzetiseg");
-                }
-                eloado.Szolo = reader.GetBoolean("Szolo");
-                eloadoLista.Add(eloado);
-            }
-            SQLConnection.Close();
 
+            BuildConnection();
+            string valasztott = "";
+            bool kilep = true;
+            do
+            {
+
+            Console.WriteLine("1. Előadók listázása");
+            Console.WriteLine("2. Előadó felvétele");
+            Console.WriteLine("3. Előadó módosítása");
+            Console.WriteLine("4. Előadó törlése");
+            Console.WriteLine("5. Kilépés");
+            valasztott = Console.ReadLine();
+            switch (valasztott)
+            {
+                case "1":
+                    List<Eloado> eloadoLista = EladoLista();
+                    foreach (var item in eloadoLista)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+                case "2":
+                        break;
+                case "3":
+                    break;
+                case "4":
+                     break;
+                case "5":
+                     kilep = false;
+                     break;
+                default:
+                    Console.WriteLine("Nincs ilyen menüpont!");
+                    break;
+            }
+            } while (kilep);
+
+
+            static List<Eloado> EladoLista()
+            {
+                List<Eloado> eloadoLista = new List<Eloado>();
+                SQLConnection.Open();
+                string sql = "SELECT * FROM eloado";
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = sql;
+                cmd.Connection = SQLConnection;
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Eloado eloado = new Eloado();
+                    eloado.Id = reader.GetInt32("Id");
+                    eloado.Nev = reader.GetString("Nev");
+                    if (!reader.IsDBNull(2))
+                    {
+                        eloado.Nemzetiseg = reader.GetString
+                            ("Nemzetiseg");
+                    }
+                    eloado.Szolo = reader.GetBoolean("Szolo");
+                    eloadoLista.Add(eloado);
+                }
+                SQLConnection.Close();
+                return eloadoLista;
+            }
         }
 
         private static void BuildConnection()
