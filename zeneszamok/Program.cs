b28200c3f93e84a1b349f229aba03998026b3626
TilajiDG <tilajid@kkszki.hu>
2025-09-03 10:28:10 +0200

    masolva a szöveg

--- zeneszamok/Program.cs
+++ zeneszamok/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace zeneszamok
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace zeneszamok
 {
     internal class Program
     {
+        static MySqlConnection SQLConnection;
         static void Main(string[] args)
         {
+            string connectionString = "SERVER = localhost;" +
+                          "DATABASE= zene;" +
+                          "UID = root;" +
+                          "PASSWORD =;" +
+                          "SSL MODE= none;";
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = connectionString;
+            conn.Open();
+            string sql = "SELECT * FROM előadó";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = sql;
+            cmd.Connection = conn;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                //A beolvasott adatok feldolgozása
+
+            }
+            conn.Close();
 
         }
     }
